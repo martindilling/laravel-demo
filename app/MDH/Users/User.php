38d4aa7d24dfc7551f4f7f431d54111406628600
Martin Dilling-Hansen <martindilling@gmail.com>
2014-09-06 12:43:52 +0200

    Namespace and cleanup User model

--- app/MDH/Users/User.php
+++ app/MDH/Users/User.php
@@ -1,33 +1,35 @@
-<?php
+<?php namespace MDH\Users;
 
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Eloquent;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface
+{
 
-	use UserTrait, RemindableTrait;
+    use UserTrait, RemindableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password', 'remember_token');
 
-	/**
-	 * The attributes that can be mass assigned.
-	 *
-	 * @var array
-	 */
-	protected $fillable = array('firstname', 'lastname', 'email', 'password');
+    /**
+     * The attributes that can be mass assigned.
+     *
+     * @var array
+     */
+    protected $fillable = array('firstname', 'lastname', 'email', 'password');
 
 }
